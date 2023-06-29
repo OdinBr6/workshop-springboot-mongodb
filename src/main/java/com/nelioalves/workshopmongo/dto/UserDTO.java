3c5bc5bd0ce3cbc0a8d47a50c68ae1e271867f11
CristianRamos <crisramosrodrigues@hotmail.com>
2023-06-29 19:51:58 -0300

    User find by id

--- src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.dto;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 import com.nelioalves.workshopmongo.domain.User;
 
@@ -11,7 +12,7 @@ public class UserDTO implements Serializable{
 	private String name;
 	private String email;
 	
-	public UserDTO() {
+	public UserDTO(Optional<User> obj) {
 	}
 	
 	public UserDTO(User obj) {
